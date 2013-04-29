ba81b342bc8b9305e6b385c7421c8a4331d1222f
Rebecca Hao <rhao@usc.edu>
2013-04-28 17:34:43 -0700

    Prepared my repository. About to reclone. Close #33. Close #34.

--- game.cpp
+++ game.cpp
@@ -1,7 +1,15 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 
-/**  Main */
+/** @mainpage CSCI 102 Programming Assignment 5
+@author Rebecca Hao
+@section purpose Purpose/Overview
+Space Invasion is a game in which an astronaut navigates his way through space. The astronaut must
+try to collect points by colliding with coins and money bags and try to stay alive by avoiding
+aliens and colliding with doctors. Turtles slow down the pace of the game, which is helpful as
+the speed of the game increases as the game goes on. The arrow keys are used to move the astronaut
+up and down as he floats through space.
+*/
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
